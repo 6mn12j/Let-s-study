373dc48eb3aa9fd9d17f62b2b09913a8d872e235
6mn12j <dbwk0404@hanmail.net>
2021-07-16 22:45:01 +0900

    Add: Router Module

--- word-cloud/word-cloud/src/App.js
+++ word-cloud/word-cloud/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import {HashRouter  , Route, Link}from "react-router-dom";
 import AppShell from "./AppShell";
+import Home from "./Home";
+import Texts from "./Texts";
+import Words from "./Words";
+
 function App()
 {
   return(
-    <AppShell />
+      <HashRouter>
+          <AppShell>
+          <div>
+            <Route  exact path="/" component={Home}/>
+            <Route  exact path="/texts" component={Texts}/>
+            <Route  exact path="/words" component={Words}/>
+          </div>
+        </AppShell>
+      </HashRouter>
+
   );
 }
 
---
+++ word-cloud/word-cloud/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+class Home extends React.Component {
+    render(){
+        return (
+            <Card>
+                <CardContent>react 및 firebase 기반의 어플리케이션</CardContent>
+            </Card>
+        )
+    };
+}
+
+export default Home;
